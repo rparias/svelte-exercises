8f002e1fd4f3921fc43b67471672d6fcd0d5010c
Ronald Arias <ronald.arias@thoughtworks.com>
2021-03-01 09:38:04 -0500

    Increment counter by clicking a button

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,8 +1,17 @@
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/svelte';
+import { fireEvent, render } from '@testing-library/svelte';
 import App from './App.svelte';
 
 test('shows proper heading when rendered', () => {
     const { getByText } = render(App, { name: 'World' });
     expect(getByText('Hello World!')).toBeInTheDocument();
+})
+
+test('Count age increments when button is clicked', async () => {
+    const { getByText } = render(App);
+    const text = getByText('My age is 1');
+    const button = getByText('Increase age');
+
+    await fireEvent.click(button);
+    expect(text).toHaveTextContent('My age is 2');
 })
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,35 @@
 <script lang="ts">
-	export let name: string;
+  export let name: string;
+  let age: number = 1;
+  const increaseAge = () => {
+    age += 1;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <h2>My age is {age}</h2>
+  <button on:click={increaseAge}>Increase age</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
