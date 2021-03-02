7ca03b4a6dffb69bf88584aebf8a01d566c9462e
Ronald Arias <ronald.arias@thoughtworks.com>
2021-03-02 12:15:22 -0500

    CSS Added dynamically

--- src/App.svelte
+++ src/App.svelte
@@ -12,9 +12,7 @@
 
   $: uppercaseName = props.name.toUpperCase();
 
-  $: if (props.name === 'Paul') {
-    console.log('It runs!');
-  }
+  $: magicProperty = props.name === 'Magic Ron' && 'magic-color';
 
   const increaseAge = () => {
     age += 1;
@@ -31,7 +29,7 @@
 </script>
 
 <main>
-  <h1>Hello {uppercaseName || 'friend'}!</h1>
+  <h1 class={magicProperty}>Hello {uppercaseName || 'friend'}!</h1>
   <h2>My age is {age}</h2>
   <button on:click={increaseAge}>Increase age</button>
   <button on:click={changeName}>Change name</button>
@@ -93,6 +91,10 @@
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
+  }
+
+  .magic-color {
+    color: #3498db;
   }
 
   @media (min-width: 640px) {
